5be5420e0e48c52a418c2385dfd8a01a21571041
mmqmzk <mmqmzk@gmail.com>
2019-04-11 16:44:53 +0800

    update

--- init.sh
+++ init.sh
@@ -1,37 +1,37 @@
 #!/usr/bin/env bash
 export PROXY=$@
-cd $(dirname $0)
+cd $(dirname "$0")
 if [[ ! -f ./functions.sh ]]; then
     echo "functions.sh not found"
     exit 1
 fi
 source ./functions.sh
 
-if [[ -z $PM ]]; then
+if [[ -z "$PM" ]]; then
   echo "yum and apt not found"
   exit 1
 fi
 
 if ! has git; then
   echo "Installing git"
-  sudo $PM install git -y
+  sudo ${PM} install git -y
 fi
 
 PY=$(wh python3 python)
-if [[ -n $PY ]]
+if [[ -n "$PY" ]]
 then
     PIP=$(wh pip3 pip) 
-    if [[ -z $PIP ]]
+    if [[ -z "$PIP" ]]
     then
       echo "Installing pip"
       GPY="/tmp/get-pip.py"
-      del $GPY
-      curl $PROXY -sSfL https://bootstrap.pypa.io/get-pip.py > $GPY
-      $PY $GPY
+      del "$GPY"
+      curl ${PROXY} -sSfL https://bootstrap.pypa.io/get-pip.py > "$GPY"
+      ${PY} ${GPY}
     fi
-    $PIP install pip -U 
+    ${PIP} install pip -U 
     echo "Installing httpie"
-    $PIP install httpie -U 
+    ${PIP} install httpie -U 
 fi
 
 if ! has ag; then
@@ -41,36 +41,36 @@ if ! has ag; then
   else
       AG="the_silver_searcher"
   fi
-  sudo $PM install $AG -y
+  sudo ${PM} install ${AG} -y
 fi
 
 if ! has zsh; then
   echo "Installing zsh"
-  sudo $PM install zsh -y
+  sudo ${PM} install zsh -y
 fi
 
 check_bin
 install_dot
 
-OMZ=$DOT/oh-my-zsh
-ZRC=~/.zshrc
-del $ZRC
+OMZ="$DOT/oh-my-zsh"
+ZRC="$HOME/.zshrc"
+del "$ZRC"
 echo "Installing oh my zsh"
-ln -s -f $DOT/zshrc $ZRC
+ln -s -f "$DOT/zshrc" "$ZRC"
 
 if ! has tmux; then
     echo "Installing tmux"
-    sudo $PM install tmux -y
+    sudo ${PM} install tmux -y
 fi
 TC=~/.tmux.conf
-del $TC
-ln -s -f $DOT/tmux.conf $TC
+del "$TC"
+ln -s -f "$DOT/tmux.conf" "$TC"
 
 
 echo "Installing git config"
 GC=~/.gitconfig
-del $GC
-ln -s -f $DOT/gitconfig  $GC
+del "$GC"
+ln -s -f "$DOT/gitconfig"  "$GC"
 
 install_bat "v0.10.0"
 
--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 PROXY=$@
 INIT=/tmp/init.sh
-curl $PROXY -fsSL https://github.com/mmqmzk/dotfiles/raw/master/functions.sh > /tmp/functions.sh
-curl $PROXY -fsSL https://github.com/mmqmzk/dotfiles/raw/master/init.sh > $INIT
-chmod 755 $INIT
-$INIT $PROXY
+curl ${PROXY} -fsSL https://github.com/mmqmzk/dotfiles/raw/master/functions.sh > /tmp/functions.sh
+curl ${PROXY} -fsSL https://github.com/mmqmzk/dotfiles/raw/master/init.sh > "$INIT"
+chmod 755 "$INIT"
+${INIT} ${PROXY}
