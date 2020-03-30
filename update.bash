7eca2f6db422761aa5ff5169f232efe0fc97920e
mmqmzk <mmqmzk@gmail.com>
2020-03-30 17:49:55 +0800

    update to latest

--- update.bash
+++ update.bash
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
-if [[ -z $1 ]]; then
-    echo "Usage: $0 <module> <version|init|noinit>"
+MOD=${1:-all}
+case "$MOD" in
+  h|help|-h|--help)
+    echo "Usage: $0 <module|all> <version|init|noinit>"
     exit 1
-fi
-cd $(dirname $0)
-MOD=$1
+    ;;
+esac
 VERSION=$2
+MODULES=(bat exa fd fzf lsd q ripgrep xsv)
+pushd $(dirname $0) &> /dev/null
 source ./functions.bash
-"install_$MOD" "$VERSION"
+if [[ "${MOD,,}" == "all" ]]; then
+  for mod in "${MODULES[@]}"; do
+    "install_$mod" ${VERSION}
+  done
+  install_node --lts
+else
+  "install_$MOD" ${VERSION}
+fi
+popd &> /dev/null
