5ab483cc57eac5c4a6496e8d6954a2ada0a84da1
mmqmzk <mmqmzk@gmail.com>
2020-04-20 13:37:09 +0800

    syncdot

--- syncdot.sh
+++ syncdot.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+set -e
+
+sudo apt update && sudo apt upgrade -y
+
 pushd "${DOT:-"$HOME/.dotfiles"}"
 git pull && git submodule update --init
 bash update.bash "$@"
@@ -16,6 +20,19 @@ aria2="$cfg/aria2"
 if [[ -e "$aria2" ]]; then
   pushd "$aria2" && git pull
   : "$linux" && [[ -d "$_" ]] && rsync -a --delete -C "$aria2" "$_"
+  popd
+fi
+lib="$HOME/.local/lib"
+scripts="$lib/scripts"
+if [[ -e "$scripts" ]]; then
+  pushd "$scripts" && git pull
+  : "$linux/bin" && [[ -d "$_" ]] && rsync -a --delete -C "$scripts/" "$_"
+  popd
+fi
+wsl_scripts="$lib/wsl-scripts"
+if [[ -e "$wsl_scripts" ]]; then
+  pushd "$wsl_scripts" && git pull
+  : "$linux/wsl" && [[ -d "$_" ]] && rsync -a --delete -C "$wsl_scripts/" "$_"
   popd
 fi
 copyq="$cfg/copyq"
