29c3a4d610c4a0b763b8390b2c56f5d9de753b2c
mmqmzk <mmqmzk@gmail.com>
2020-04-20 14:06:39 +0800

    syncdot

--- syncdot.sh
+++ syncdot.sh
@@ -2,42 +2,46 @@
 
 set -e
 
-sudo apt update && sudo apt upgrade -y
+sudo bash -c "${PM_UPDATE:-"apt update && apt upgrade -y"}"
 
 pushd "${DOT:-"$HOME/.dotfiles"}"
 git pull && git submodule update --init
 bash update.bash "$@"
 popd
+
 linux="$GOOGLE_DRIVE/Config/Linux"
 cfg="${XDG_CONFIG_HOME:-"$HOME/.config"}"
 ranger="$cfg/ranger"
 if [[ -e "$ranger" ]]; then
   pushd "$ranger" && git pull
-  : "$linux" && [[ -d "$_" ]] && rsync -a --delete -C "$ranger" "$_"
+  : "$linux" && [[ -d "$_" ]] && rsync -aCh --delete --progress \
+    -f "-s __pycache__/" "$ranger" "$_"
   popd
 fi
 aria2="$cfg/aria2"
 if [[ -e "$aria2" ]]; then
   pushd "$aria2" && git pull
-  : "$linux" && [[ -d "$_" ]] && rsync -a --delete -C "$aria2" "$_"
+  : "$linux" && [[ -d "$_" ]] && rsync -aCh --delete --progress "$aria2" "$_"
   popd
 fi
 lib="$HOME/.local/lib"
 scripts="$lib/scripts"
 if [[ -e "$scripts" ]]; then
   pushd "$scripts" && git pull
-  : "$linux/bin" && [[ -d "$_" ]] && rsync -a --delete -C "$scripts/" "$_"
+  : "$linux/bin" && [[ -d "$_" ]] && rsync -aCh --delete \
+    --progress "$scripts/" "$_"
   popd
 fi
 wsl_scripts="$lib/wsl-scripts"
 if [[ -e "$wsl_scripts" ]]; then
   pushd "$wsl_scripts" && git pull
-  : "$linux/wsl" && [[ -d "$_" ]] && rsync -a --delete -C "$wsl_scripts/" "$_"
+  : "$linux/wsl" && [[ -d "$_" ]] && rsync -aCh --delete \
+    --progress "$wsl_scripts/" "$_"
   popd
 fi
 copyq="$cfg/copyq"
 if [[ -e "$copyq" && -d "$linux" ]]; then
-  rsync -a --delete -C "$copyq" "$linux"
+  rsync -aCh --delete --progress "$copyq" "$linux"
 fi
 gist="$HOME/.local/lib/gist"
 if [[ -d "$gist" ]]; then
